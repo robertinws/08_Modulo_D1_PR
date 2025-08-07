fef776dfdb7a3822959aba1bc7a46353b8328588
robertinws <bertinws@gmail.com>
2025-08-07 13:41:08 -0300

    Controller criado

---
+++ lib/app_controller.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:modulo_d1_pr/home_page.dart';
+
+class AppController extends StatelessWidget {
+  const AppController({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {'/': (context) => HomePage()},
+    );
+  }
+}
