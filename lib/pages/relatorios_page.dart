4ac267a97f8fbb1d8dbc7a0157da20b44573e6c3
robertinws <bertinws@gmail.com>
2025-09-10 16:41:46 -0300

    Tela relatorios design comecado

--- lib/pages/relatorios_page.dart
+++ lib/pages/relatorios_page.dart
@@ -1,3 +1,5 @@
+import 'package:_08_modulo_d1_pr/components/bottom_nav_comp.dart';
+import 'package:_08_modulo_d1_pr/global/colors.dart';
 import 'package:flutter/material.dart';
 
 class RelatoriosPage extends StatefulWidget {
@@ -10,6 +12,27 @@ class RelatoriosPage extends StatefulWidget {
 class _RelatoriosPageState extends State<RelatoriosPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: corClara,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        scrolledUnderElevation: 0,
+        title: Text('Relatórios'),
+        leading: IconButton(
+          onPressed: () {
+            Navigator.of(
+              context,
+            ).pushNamedAndRemoveUntil('/', (context) => false);
+          },
+          icon: Icon(Icons.home, size: 40),
+        ),
+      ),
+      floatingActionButton: IconButton(
+        onPressed: () {},
+        icon: Icon(Icons.save),
+      ),
+      bottomNavigationBar: BottomNavComp(contexto: context),
+    );
   }
 }
